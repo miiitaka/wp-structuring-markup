c98dc098a2423c8b5f900515f104715289d7b2e0
Kazuya Takami <foundationmeister@outlook.com>
2015-12-01 19:12:51 +0900

    Uninstall update. Delete data of custom post "event".

--- uninstall.php
+++ uninstall.php
@@ -8,7 +8,7 @@ new Structuring_Markup_Uninstall();
  * Schema.org Plugin Uninstall
  *
  * @author  Kazuya Takami
- * @version 1.0.0
+ * @version 2.1.0
  * @since   1.0.0
  */
 class Structuring_Markup_Uninstall {
@@ -20,18 +20,56 @@ class Structuring_Markup_Uninstall {
 	 * @version 2.0.0
 	 */
 	function __construct() {
-		$this->drop_table();
+		$this->drop_table( 'structuring_markup' );
+		$this->delete_custom_post( 'schema_event_post' );
+		$this->delete_post_meta( 'schema_event_post' );
 		delete_option( 'wp_structuring_markup' );
 	}
 
 	/**
 	 * Drop Table.
 	 *
-	 * @since 1.0.0
+	 * @since   1.0.0
+	 * @version 2.1.0
+	 * @param   string $table_name
 	 */
-	private function drop_table() {
+	private function drop_table( $table_name = null ) {
 		global $wpdb;
-		$table_name = $wpdb->prefix . "structuring_markup";
+		$table_name = $wpdb->prefix . $table_name;
 		$wpdb->query( "DROP TABLE IF EXISTS " . $table_name );
+	}
+
+	/**
+	 * Delete custom post.
+	 *
+	 * @since   2.1.0
+	 * @version 2.1.0
+	 * @param   string $custom_type
+	 */
+	private function delete_custom_post( $custom_type = null ) {
+		global $wpdb;
+		$table_name = $wpdb->prefix . "post";
+
+		$key = array( 'post_type' => $custom_type );
+		$key_prepared = array( '%s' );
+
+		$wpdb->delete( $table_name, $key, $key_prepared );
+	}
+
+	/**
+	 * Delete post meta.
+	 *
+	 * @since   2.1.0
+	 * @version 2.1.0
+	 * @param   string $custom_type
+	 */
+	private function delete_post_meta( $custom_type = null ) {
+		global $wpdb;
+		$table_name = $wpdb->prefix . "postmeta";
+
+		$key = array( 'meta_key' => $custom_type );
+		$key_prepared = array( '%s' );
+
+		$wpdb->delete( $table_name, $key, $key_prepared );
 	}
 }
