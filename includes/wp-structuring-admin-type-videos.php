3d4785405a297f8f73b06ea8fdaea5c2fa1afd10
Kazuya Takami <foundationmeister@outlook.com>
2016-06-07 20:21:31 +0900

    Updated : video post.

--- includes/wp-structuring-admin-type-videos.php
+++ includes/wp-structuring-admin-type-videos.php
@@ -28,9 +28,14 @@ class Structuring_Markup_Type_Videos {
 	private function page_render () {
 		$html  = '<table class="schema-admin-table">';
 		$html .= '<caption>Basic Setting</caption>';
-		$html .= '<tr><th>name :</th><td><small>Post title</small></td></tr>';
-		$html .= '<tr><th>Description :</th><td><small>Post description</small></td></tr>';
-		$html .= '<tr><th>thumbnailUrl :</th><td><small>Input a custom post: field name "schema_event_url"</small></td></tr>';
+		$html .= '<tr><th>name :</th><td><small>Post Title</small></td></tr>';
+		$html .= '<tr><th>Description :</th><td><small>Post Description</small></td></tr>';
+		$html .= '<tr><th>thumbnailUrl :</th><td><small>Featured Image URL</small></td></tr>';
+		$html .= '<tr><th>uploadDate :</th><td><small>Update Date</small></td></tr>';
+		$html .= '<tr><th>duration :</th><td><small>Input a custom post: field name "schema_video_duration"</small></td></tr>';
+		$html .= '<tr><th>contentUrl :</th><td><small>Input a custom post: field name "schema_video_content_url"</small></td></tr>';
+		$html .= '<tr><th>embedUrl :</th><td><small>Input a custom post: field name "schema_video_embed_url"</small></td></tr>';
+		$html .= '<tr><th>interactionCount :</th><td><small>Input a custom post: field name "schema_video_interaction_count"</small></td></tr>';
 		$html .= '</table>';
 		echo $html;
 
