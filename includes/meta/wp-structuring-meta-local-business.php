33702ef20ec3b2ef1bfecc0cd884d365d80aff0e
Kazuya Takami <foundationmeister@outlook.com>
2018-09-19 15:48:15 +0900

    Organization schema.org type subdivision. #362

--- includes/meta/wp-structuring-meta-local-business.php
+++ includes/meta/wp-structuring-meta-local-business.php
@@ -3,7 +3,7 @@
  * Schema.org Type LocalBusiness
  *
  * @author  Kazuya Takami
- * @version 4.1.6
+ * @version 4.6.0
  * @since   4.0.0
  * @see     wp-structuring-opening-hours.php
  * @link    http://schema.org/LocalBusiness
@@ -15,7 +15,7 @@ class Structuring_Markup_Meta_LocalBusiness {
 	/**
 	 * Setting schema.org LocalBusiness
 	 *
-	 * @version 4.1.6
+	 * @version 4.6.0
 	 * @since   4.0.0
 	 * @param   array $options
 	 * @return  array $args
@@ -24,21 +24,21 @@ class Structuring_Markup_Meta_LocalBusiness {
 
 		/** weekType defined. */
 		$week_array = array(
-			array( "type" => "Mo", "display" => "Monday" ),
-			array( "type" => "Tu", "display" => "Tuesday" ),
-			array( "type" => "We", "display" => "Wednesday" ),
-			array( "type" => "Th", "display" => "Thursday" ),
-			array( "type" => "Fr", "display" => "Friday" ),
-			array( "type" => "Sa", "display" => "Saturday" ),
-			array( "type" => "Su", "display" => "Sunday" )
+			array( 'type' => 'Mo', 'display' => 'Monday' ),
+			array( 'type' => 'Tu', 'display' => 'Tuesday' ),
+			array( 'type' => 'We', 'display' => 'Wednesday' ),
+			array( 'type' => 'Th', 'display' => 'Thursday' ),
+			array( 'type' => 'Fr', 'display' => 'Friday' ),
+			array( 'type' => 'Sa', 'display' => 'Saturday' ),
+			array( 'type' => 'Su', 'display' => 'Sunday' )
 		);
 
 		$args = array(
-			"@context"  => "http://schema.org",
-			"@type"     => isset( $options['business_type'] ) ? esc_html( $options['business_type'] ) : "",
-			"name"      => isset( $options['name'] )          ? esc_html( $options['name'] ) : "",
-			"image"     => isset( $options['image'] )         ? esc_html( $options['image'] ) : "",
-			"url"       => isset( $options['url'] )           ? esc_url( $options['url'] ) : ""
+			'@context'  => 'http://schema.org',
+			'@type'     => isset( $options['business_type'] ) ? esc_html( $options['business_type'] ) : 'LocalBusiness',
+			'name'      => isset( $options['name'] )          ? esc_html( $options['name'] ) : '',
+			'image'     => isset( $options['image'] )         ? esc_html( $options['image'] ) : '',
+			'url'       => isset( $options['url'] )           ? esc_url( $options['url'] ) : ''
 		);
 
 		if ( isset( $options['telephone'] ) && !empty( $options['telephone'] ) ) {
@@ -50,9 +50,9 @@ class Structuring_Markup_Meta_LocalBusiness {
 				$args['menu'] = esc_url( $options['menu'] );
 			}
 			if ( isset( $options['accepts_reservations'] ) && $options['accepts_reservations'] === 'on' ) {
-				$args['acceptsReservations'] = "True";
+				$args['acceptsReservations'] = 'True';
 			} else {
-				$args['acceptsReservations'] = "False";
+				$args['acceptsReservations'] = 'False';
 			}
 			if ( isset( $options['serves_cuisine'] ) && $options['serves_cuisine'] !== '' ) {
 				$args['servesCuisine'] = esc_html( $options['serves_cuisine'] );
@@ -60,11 +60,11 @@ class Structuring_Markup_Meta_LocalBusiness {
 		}
 
 		$address_array['address'] = array(
-			"@type"           => "PostalAddress",
-			"streetAddress"   => isset( $options['street_address'] )   ? esc_html( $options['street_address'] ) : "",
-			"addressLocality" => isset( $options['address_locality'] ) ? esc_html( $options['address_locality'] ) : "",
-			"postalCode"      => isset( $options['postal_code'] )      ? esc_html( $options['postal_code'] ) : "",
-			"addressCountry"  => isset( $options['address_country'] )  ? esc_html( $options['address_country'] ) : ""
+			'@type'           => 'PostalAddress',
+			'streetAddress'   => isset( $options['street_address'] )   ? esc_html( $options['street_address'] ) : '',
+			'addressLocality' => isset( $options['address_locality'] ) ? esc_html( $options['address_locality'] ) : '',
+			'postalCode'      => isset( $options['postal_code'] )      ? esc_html( $options['postal_code'] ) : '',
+			'addressCountry'  => isset( $options['address_country'] )  ? esc_html( $options['address_country'] ) : ''
 		);
 
 		if ( isset( $options['address_region'] ) && !empty( $options['address_region'] ) ) {
@@ -75,21 +75,21 @@ class Structuring_Markup_Meta_LocalBusiness {
 		$geo_array = array();
 
 		if ( isset( $options['geo_active'] ) && $options['geo_active'] === 'on' ) {
-			$geo_array["geo"] = array(
-				"@type"     => "GeoCoordinates",
-				"latitude"  => isset( $options['latitude'] ) ? esc_html(floatval($options['latitude'])) : "",
-				"longitude" => isset( $options['longitude'] ) ? esc_html(floatval($options['longitude'])) : ""
+			$geo_array['geo'] = array(
+				'@type'     => 'GeoCoordinates',
+				'latitude'  => isset( $options['latitude'] )  ? esc_html(floatval($options['latitude'])) : '',
+				'longitude' => isset( $options['longitude'] ) ? esc_html(floatval($options['longitude'])) : ''
 			);
 		}
 
 		if ( isset( $options['geo_circle_active'] ) && $options['geo_circle_active'] === 'on' ) {
-			$place_array["location"]        = array( "@type" => "Place" );
-			$place_array["location"]["geo"] = array(
-				"@type"     => "GeoCircle",
-				"geoRadius" => isset( $options['geo_circle_radius'] )  ? esc_html( floatval( $options['geo_circle_radius'] ) ) : ""
+			$place_array['location']        = array( '@type' => 'Place' );
+			$place_array['location']['geo'] = array(
+				'@type'     => 'GeoCircle',
+				'geoRadius' => isset( $options['geo_circle_radius'] )  ? esc_html( floatval( $options['geo_circle_radius'] ) ) : ''
 			);
 			if ( isset( $options['geo_active'] ) && $options['geo_active'] === 'on' ) {
-				$place_array["location"]["geo"]["geoMidpoint"] = $geo_array["geo"];
+				$place_array['location']['geo']['geoMidpoint'] = $geo_array['geo'];
 			}
 			$args = array_merge( $args, $place_array );
 		} else {
@@ -111,10 +111,10 @@ class Structuring_Markup_Meta_LocalBusiness {
 			$obj = new Structuring_Markup_Opening_Hours( $active_days );
 			$opening_hours = $obj->display();
 
-			$opening_array["openingHours"] = array();
+			$opening_array['openingHours'] = array();
 
 			foreach( $opening_hours as $value ) {
-				$opening_array["openingHours"][] = $value;
+				$opening_array['openingHours'][] = $value;
 			}
 
 			$args = array_merge( $args, $opening_array );
@@ -122,18 +122,18 @@ class Structuring_Markup_Meta_LocalBusiness {
 		}
 
 		if ( isset( $options['holiday_active'] ) && $options['holiday_active'] === 'on' ) {
-			$holiday_array["openingHoursSpecification"] = array(
-				"@type"        => "OpeningHoursSpecification",
-				"opens"        => isset( $options['holiday_open'] ) ? esc_html( $options['holiday_open'] ) : "",
-				"closes"       => isset( $options['holiday_close'] ) ? esc_html( $options['holiday_close'] ) : "",
-				"validFrom"    => isset( $options['holiday_valid_from'] ) ? esc_html( $options['holiday_valid_from'] ) : "",
-				"validThrough" => isset( $options['holiday_valid_through'] ) ? esc_html( $options['holiday_valid_through'] ) : ""
+			$holiday_array['openingHoursSpecification'] = array(
+				'@type'        => 'OpeningHoursSpecification',
+				'opens'        => isset( $options['holiday_open'] )          ? esc_html( $options['holiday_open'] ) : '',
+				'closes'       => isset( $options['holiday_close'] )         ? esc_html( $options['holiday_close'] ) : '',
+				'validFrom'    => isset( $options['holiday_valid_from'] )    ? esc_html( $options['holiday_valid_from'] ) : '',
+				'validThrough' => isset( $options['holiday_valid_through'] ) ? esc_html( $options['holiday_valid_through'] ) : ''
 			);
 			$args = array_merge( $args, $holiday_array );
 		}
 
 		if ( isset( $options['price_range'] ) && !empty( $options['price_range'] ) ) {
-			$price_array["priceRange"] = $options['price_range'];
+			$price_array['priceRange'] = $options['price_range'];
 			$args = array_merge( $args, $price_array );
 		}
 
--- includes/meta/wp-structuring-meta-organization.php
+++ includes/meta/wp-structuring-meta-organization.php
@@ -3,7 +3,7 @@
  * Schema.org Type Organization
  *
  * @author  Kazuya Takami
- * @version 4.1.3
+ * @version 4.6.0
  * @since   4.0.0
  * @link    https://schema.org/Organization
  * @link    https://developers.google.com/search/docs/guides/enhance-site
@@ -13,7 +13,7 @@ class Structuring_Markup_Meta_Organization {
 	/**
 	 * Setting schema.org Organization
 	 *
-	 * @version 4.1.3
+	 * @version 4.6.0
 	 * @since   4.0.0
 	 * @param   array $options
 	 * @return  array $args
@@ -21,28 +21,28 @@ class Structuring_Markup_Meta_Organization {
 	public function set_meta ( array $options ) {
 		/** Logos */
 		$args = array(
-			"@context" => "http://schema.org",
-			"@type"    => "Organization",
-			"name"     => isset( $options['name'] ) ? esc_html( $options['name'] ) : "",
-			"url"      => isset( $options['url'] )  ? esc_url( $options['url'] ) : "",
-			"logo"     => isset( $options['logo'] ) ? esc_url( $options['logo'] ) : ""
+			'@context' => 'http://schema.org',
+			'@type'    => isset( $options['organization_type'] ) ? esc_html( $options['organization_type'] ) : 'Organization',
+			'name'     => isset( $options['name'] )              ? esc_html( $options['name'] ) : '',
+			'url'      => isset( $options['url'] )               ? esc_url( $options['url'] ) : '',
+			'logo'     => isset( $options['logo'] )              ? esc_url( $options['logo'] ) : ''
 		);
 
 		/** Corporate Contact */
 		if ( isset( $options['contact_point'] ) && $options['contact_point'] === 'on' ) {
 			$contact_point_data = array(
-				"@type"       => "ContactPoint",
-				"telephone"   => isset( $options['telephone'] )    ? esc_html( $options['telephone'] ) : "",
-				"contactType" => isset( $options['contact_type'] ) ? esc_html( $options['contact_type'] ) : ""
+				'@type'       => 'ContactPoint',
+				'telephone'   => isset( $options['telephone'] )    ? esc_html( $options['telephone'] ) : '',
+				'contactType' => isset( $options['contact_type'] ) ? esc_html( $options['contact_type'] ) : ''
 			);
 
 			if ( !empty( $options['email'] ) ) {
-				$contact_point_data['email'] = isset( $options['email'] ) ? esc_html( $options['email'] ) : "";
+				$contact_point_data['email'] = isset( $options['email'] ) ? esc_html( $options['email'] ) : '';
 			}
 			if ( !empty( $options['area_served'] ) ) {
 				$array = explode( ',', esc_html(  $options['area_served'] ) );
 				for ( $i = 0; $i < count( $array ); $i++ ) {
-					$contact_point_data['areaServed'][] = isset( $options['area_served'] ) ? $array[$i] : "";
+					$contact_point_data['areaServed'][] = isset( $options['area_served'] ) ? $array[$i] : '';
 				}
 			}
 			if ( isset( $options['contact_point_1'] ) &&  $options['contact_point_1'] === 'on' ) {
@@ -54,24 +54,24 @@ class Structuring_Markup_Meta_Organization {
 			if ( !empty( $options['available_language'] ) ) {
 				$array = explode( ',', esc_html( $options['available_language'] ) );
 				for ( $i = 0; $i < count( $array ); $i++ ) {
-					$contact_point_data['availableLanguage'][] = isset( $options['available_language'] ) ? $array[$i] : "";
+					$contact_point_data['availableLanguage'][] = isset( $options['available_language'] ) ? $array[$i] : '';
 				}
 			}
 
-			$contact_point["contactPoint"] = array( $contact_point_data	);
+			$contact_point['contactPoint'] = array( $contact_point_data	);
 			$args = array_merge( $args, $contact_point );
 		}
 
 		/** Social Profiles */
 		if ( isset( $options['social'] ) ) {
-			$socials["sameAs"] = array();
+			$socials['sameAs'] = array();
 
 			foreach ( $options['social'] as $value ) {
 				if ( $value ) {
-					$socials["sameAs"][] = esc_url( $value );
+					$socials['sameAs'][] = esc_url( $value );
 				}
 			}
-			if ( count( $socials["sameAs"] ) > 0 ) {
+			if ( count( $socials['sameAs'] ) > 0 ) {
 				$args = array_merge( $args, $socials );
 			}
 		}
